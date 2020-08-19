e6f60b10a22f13c2c1792ef8b2721dcee082375c
chefkasperson <chefkasperson@gmail.com>
2020-08-19 10:49:50 -0700

    Updates

---
+++ precache-manifest.ad4454ba6f997c1b1349a8aa20e2590e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae300f81a24ee3cf7d66dbc0ff628d83",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1897319c472346559758",
+    "url": "/monster-rolodex/static/css/main.eba15a7c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1897319c472346559758",
+    "url": "/monster-rolodex/static/js/main.6935bdf5.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
